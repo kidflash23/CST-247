9f708d49f4055f6da4b3c450fc2f6ac31c121fb8
Ray Omoregie <42901966+kidflash23@users.noreply.github.com>
2020-07-30 18:56:53 -0700

    Add files via upload
    New Changes and edits for Activity 2 Assignment

---
+++ Activity Code Assignments/Models/ButtonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity2Part1.Models
+{
+    public class ButtonModel
+    {
+        public bool State { get; set; }
+        
+
+        public ButtonModel(bool state)
+        {
+            this.State = state;
+        }
+
+    }
+}
---
+++ Activity Code Assignments/README.md
@@ -0,0 +1,2 @@
+# CST-247
+This is my repository for the CST-247 course for Summer 2020 for GCU for Enterprise Applications Programming III
